e4a661628576645dc05b8477f2b90921ebd7411a
Kevin Zhang <zhangk6@rpi.edu>
2015-03-17 20:38:27 -0400

    Fixed errors in loop section

--- MIPS/hw3_2.s
+++ MIPS/hw3_2.s
@@ -26,16 +26,17 @@ main:
     syscall
     li $v0, 8           # take in input
     la $a0, strvar      # load "strvar" byte space
-    move $s0, $a0       # save to s0
     syscall
-    jal count           # call count
-    beq $s1, 0, error   # error: all whitespace
+    move $s0, $a0       # save to s0
     la $a0, mnstr2      # print mnstr2
     li $v0, 4
     syscall
     li $v0, 8           # take in input
     la $a0, serstr      # load "serstr" byte space
+    syscall
     move $s3, $a0       # save to $s3
+    jal count           # call count
+    beq $s2, 0, error   # error: all whitespace
     la $a0, res1        # print first set of results
     li $v0, 4
     syscall
@@ -48,7 +49,7 @@ main:
     la $a0, res2        # print second set of results
     li $v0, 4
     syscall
-    move $a0, $s1       # print # of non-whitespace
+    move $a0, $s2       # print # of non-whitespace
     li $v0, 1
     syscall
     la $a0, endl        # print newline
@@ -72,21 +73,20 @@ count:
     addi $t0, $0, 0
 # loop through $s0 (input string)
 loop:
-    #add $t0, $t0, $s0
-    move $t0, $s0
-    lb $t1, 0($t0)
-    beq $t1, 'o', exit
+    add $t1, $s0, $t0
+    lb $t2, 0($t1)
+    beq $t2, 0, exit
     # check for equality here - if white then restart
-    beq $t1, ' ', white
-    beq $t1, '\t', white
-    beq $t1, '\n', white
+    beq $t2, ' ', white
+    beq $t2, '\t', white
+    beq $t2, '\n', white
     # if we reached this point, it must have been non-whitespace
     addi $s2, $s2, 1    # increment $s2
     addi $s1, $s1, -1   # decrement $s1 since it's incremented again
 # case white
 white:
     addi $s1, $s1, 1    # increment $s1
-    addi $s0, $s0, 1    # point to next character
+    addi $t0, $t0, 1    # point to next character
     j loop              # jump to _loop
 # exit case
 exit:
