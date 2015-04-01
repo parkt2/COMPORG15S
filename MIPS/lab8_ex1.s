ac7e4cd1e68eaef20548e5b17d89b63e808e8cb1
Kevin Zhang <zhangk6@rpi.edu>
2015-04-01 01:03:49 -0400

    Finish lab8 ex1

--- MIPS/lab8_ex1.s
+++ MIPS/lab8_ex1.s
@@ -17,18 +17,48 @@ newline: .asciiz "\n"
 
 main:
 # Compute: (4.0 * pi) which is used for both equations
+        lwc1 $f1, four
+        lwc1 $f2, pi
+        lwc1 $f7, three
+        mul.s $f3, $f1, $f2
 
 # Prompt the user for an input radius
+        la $a0, prompt # print prompt
+        li $v0, 4
+        syscall
+        li $v0, 6
+        syscall
 
 # Calculate surface area of a sphere
 #   surface_area = 4.0 * pi * radius^2
+       mul.s $f5, $f0, $f0
+       mul.s $f12, $f3, $f5
 
 # Print surface area to user
+        la $a0, saprmpt
+        li $v0, 4
+        syscall
+        li $v0, 2
+        syscall
+        la $a0, newline
+        li $v0, 4
+        syscall
 
 # Calculate volume of a sphere:
 #   volume = (4.0 * pi / 3.0) * radius^3
+        mul.s $f4, $f5, $f0
+        div.s $f6, $f3, $f7
+        mul.s $f12, $f6, $f4
 
 # Print surface area to user
+        la $a0, vlprmpt
+        li $v0, 4
+        syscall
+        li $v0, 2
+        syscall
+        la $a0, newline
+        li $v0, 4
+        syscall
 
 # Exit program
          li      $v0, 10         # To the bat mobile
