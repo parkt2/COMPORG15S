470f8ff49e8553954b5ee2a9ad80732cf9d10387
Kevin Zhang <zhangk6@rpi.edu>
2015-04-01 10:32:53 -0400

    Fix lab8

--- MIPS/lab8_ex2.s
+++ MIPS/lab8_ex2.s
@@ -17,23 +17,24 @@ main:       move  $t0, $0        # $t0 is a loop index
             # Sum the elements in the array
 loop:       beq   $t0, $s1, compute # When $t0 = $s1, we hit end of array
             # TODO
-            lw $t2, 0($s0)
-            add $t1, $t1, $t2
-            addi $s0, $s0, 4
-            addi $t0, $t0, 1
+            lw $t2, 0($s0) # load word
+            add $t1, $t1, $t2 # add to sum
+            addi $s0, $s0, 4 # get next word
+            addi $t0, $t0, 1 # increment i (for sentinel value)
             b     loop
 
             # Compute the average using the FP coprocessor
             #    and store the result in average
 compute:
             # TODO
-            lw $t3, length
-            mtc1.d $t3, $f4
+            lw $t3, length # load length
+            mtc1.d $t3, $f4 # convert to double
             cvt.d.w $f2, $f4
-            mtc1 $t1, $f1
+            mtc1 $t1, $f1 # convert to double
             cvt.d.w $f0, $f1
-            div.d $f0, $f0, $f2
-            mov.d $f12, $f0
+            div.d $f0, $f0, $f2 # divide
+            sdc1 $f0, average # store in average
+            mov.d $f12, $f0 # move to $f12 for printing
 
             # Print out average value
             la      $a0, outstr     # Output string
