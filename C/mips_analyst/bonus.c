d1557a78e97264bd43a64325f3372877ff3b959c
Kevin Zhang <kzhaaang@gmail.com>
2015-04-29 16:36:32 -0400

    Added bonus homework (mips analyst)

---
+++ C/mips_analyst/bonus.c
@@ -0,0 +1,9 @@
+/*
+ * Author: Kevin Zhang
+ * Section: 01
+ * Title: bonus.c
+ * Comments:
+ * 
+ */
+#include <stdio.h> // general I/O functions
+#include <stdlib.h> // certain overloaded functions (malloc in particular)
