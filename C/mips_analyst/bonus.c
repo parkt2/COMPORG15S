8bb2a69109a845c07a3a97628ab0279e1ae920de
Kevin Zhang <kzhaaang@gmail.com>
2015-04-29 16:54:23 -0400

    Program outline

--- C/mips_analyst/bonus.c
+++ C/mips_analyst/bonus.c
@@ -7,3 +7,32 @@
  */
 #include <stdio.h> // general I/O functions
 #include <stdlib.h> // certain overloaded functions (malloc in particular)
+
+/* function list */
+void help(char*);
+
+int main(int argc, char **argv) {
+    /* Preliminary checks and argument handling */
+    if(argc == 1) { // if user only inputs the program name
+        help(argv[0]);
+    }
+    /* Actual execution */
+    else if(argc == 3) {
+
+    }
+    /* Errors */
+    else {
+    	printf("Error: Wrong number of arguments. Run without arguments for usage information.\n");
+    }
+    return 0;
+}
+
+/*
+ * help(char*)
+ * Prints out the help and usage information to the standard output.
+ */
+void help(char args[])  {
+    printf("Usage: %s [INFILE] [OUTFILE]\n", args);
+    printf("Summary:\n");
+    printf("Analyzes a MIPS program and prints out a reference table of identifiers.\n");
+}
